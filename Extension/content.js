f0cfcbaafb0fe372c72ba8fb8cb3b423d21423cb
Khushiimaheshwari <142376177+Khushiimaheshwari@users.noreply.github.com>
2025-02-27 23:58:57 +0530

    Delete Extension directory

--- Extension/content.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("input", function(event) {
-    const text = event.target.value || "";
-    chrome.storage.local.get(["userText"], function(result) {
-      const updatedText = (result.userText || "") + " " + text;
-      chrome.storage.local.set({ userText: updatedText });
-    });
-  });
-  
